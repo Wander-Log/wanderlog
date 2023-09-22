2bc14f5fb732434d2f3f07c1db60df1a7306aed1
BecomeWeasel <hsw0194@gmail.com>
2023-09-23 02:26:29 +0900

    lint

--- common/src/main/kotlin/com/wanderlog/common/error/ErrorType.kt
+++ common/src/main/kotlin/com/wanderlog/common/error/ErrorType.kt
@@ -4,5 +4,5 @@ import org.springframework.boot.logging.LogLevel
 import org.springframework.http.HttpStatus
 
 enum class ErrorType(val status: HttpStatus, val code: ErrorCode, val message: String, val level: LogLevel) {
-    COMMON_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, ErrorCode.E5000, "처리 중 오류가 발생했습니다.", LogLevel.ERROR),
+    COMMON_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, ErrorCode.E5000, "처리 중 오류가 발생했습니다.", LogLevel.ERROR)
 }
